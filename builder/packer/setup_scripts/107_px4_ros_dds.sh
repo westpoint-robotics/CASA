c6bb337e6815b889251c4c83b4070ec389a2cee9
Steve Henderson <steven.j.henderson@gmail.com>
2022-12-03 23:38:06 -0500

    Adding in DDS components

--- builder/packer/setup_scripts/107_px4_ros_dds.sh
+++ builder/packer/setup_scripts/107_px4_ros_dds.sh
@@ -5,11 +5,12 @@ sudo apt-get install -y libp11-dev libengine-pkcs11-openssl
 sudo apt-get install -y softhsm2
 sudo apt-get install -y openjdk-11-jdk-headless
 
+source /opt/ros/humble/setup.bash
+
 cd ~
 mkdir fastdds/
 cd fastdds
-mkdir Fast-DDS
-mkdir Fast-DDS/install
+
 git clone https://github.com/eProsima/foonathan_memory_vendor.git
 cd foonathan_memory_vendor
 mkdir build
@@ -22,24 +23,23 @@ cd ~/fastdds
 git clone https://github.com/eProsima/Fast-CDR.git
 mkdir Fast-CDR/build
 cd Fast-CDR/build
-cmake .. -DCMAKE_INSTALL_PREFIX=~/fastdds/Fast-DDS/install
+cmake .. -DCMAKE_INSTALL_PREFIX=~/fastdds/Fast-CDR/install
 cmake --build . --target install
 
 # FastDDS
 cd ~/fastdds
 git clone https://github.com/eProsima/Fast-DDS.git
-mkdir Fast-DDS/build
-cd Fast-DDS/build/
+cd Fast-DDS/src
 cmake ..  -DCMAKE_INSTALL_PREFIX=~/fastdds/Fast-DDS/install
 cmake --build . --target install
 cmake ..  -DCMAKE_INSTALL_PREFIX=~/fastdds/Fast-DDS/install -DTHIRDPARTY=ON -DSECURITY=ON 
 make -j$(nproc --all) 
 sudo make install
 
-# Fast RTPS
+# Fast DDS Gen
 cd ~/fastdds
-clone --recursive https://github.com/eProsima/Fast-DDS-Gen.git -b v1.0.4 ~/Fast-RTPS-Gen
-cd Fast-RTPS-Gen/
+git clone --recursive https://github.com/eProsima/Fast-DDS-Gen.git -b v1.0.4 ~/fastdds/Fast-DDS-Gen
+cd ~/fastdds/Fast-DDS-Gen/
 sed -i 's/5.6.2/6.8.3/g' gradle/wrapper/gradle-wrapper.properties 
 ./gradlew assemble
 sudo env "PATH=$PATH" ./gradlew install
@@ -49,9 +49,13 @@ cd ~
 mkdir ~/microros_ws
 cd microros_ws
 git clone -b humble https://github.com/micro-ROS/micro_ros_setup.git 
+mkdir src
+mv micro_ros_setup/ src/
+colcon build
+source install/local_setup.sh
 ros2 run micro_ros_setup create_agent_ws.sh
 ros2 run micro_ros_setup build_agent.sh
-source install/local_setup.sh
+
 
 # px4_ros_com and px4_msgs
 cd ~
