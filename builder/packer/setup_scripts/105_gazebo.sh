38f8d4973b0e5107be4014e5068f43935a102a0d
Steve Henderson <steven.j.henderson@gmail.com>
2022-12-04 13:35:39 -0500

    Trying to get gazebo

--- builder/packer/setup_scripts/105_gazebo.sh
+++ builder/packer/setup_scripts/105_gazebo.sh
@@ -1,1 +1,5 @@
-curl -sSL http://get.gazebosim.org | sh
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+wget https://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install gazebo
+sudo apt-get install libgazebo11-dev
