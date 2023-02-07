e3d11d9bceb29ffb69d27c500e9089de5b67310f
Jason <jhughes50@fordham.edu>
2023-02-06 20:32:36 -0500

    system interface subscription

--- src/interfaces/system_interface/src/system.cpp
+++ src/interfaces/system_interface/src/system.cpp
@@ -13,11 +13,59 @@
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp/qos.hpp>
 
-#inlcude "px4_msgs/msg/vehicle_status.hpp"
+#include "px4_msgs/msg/vehicle_status.hpp"
 
-#include "system_interface/system.hpp"
+//#include "system_interface/system.hpp"
 
 class SystemInterface : public rclcpp::Node
 {
 public:
   SystemInterface() : Node("system_interface")
+  {
+    rclcpp::QoS qos(10);
+    qos.keep_last(10);
+    qos.best_effort();
+    qos.transient_local();
+
+    //declare parameters below
+
+    //rclcpp::executors::SingleThreadExecutor exec;
+    //std::vector<rclcpp::Node::SharedPtr> node_references;
+    //std::vector<rclcpp::Subscription<px4_msgs::msg::VehicleStatus>::SharedPtr> sub_references;
+
+    for (int i = 0; i < 2; i++)
+      {
+	std::string topic = "px4_"+ std::to_string(i) + "/fmu/out/vehicle_status";
+	auto sub = this -> create_subscription<px4_msgs::msg::VehicleStatus>(topic,
+									     qos,
+									     std::bind(&SystemInterface::status_callback,
+										       this,
+										       std::placeholders::_1));
+
+	sub_references.push_back(sub);
+	
+      }
+  }
+
+private:
+
+  std::vector<rclcpp::Subscription<px4_msgs::msg::VehicleStatus>::SharedPtr> sub_references;
+
+  void status_callback(const px4_msgs::msg::VehicleStatus& msg);
+
+};
+
+void SystemInterface::status_callback(const px4_msgs::msg::VehicleStatus& msg)
+{
+  RCLCPP_INFO_STREAM(this->get_logger(), "message recieved");
+}
+
+int main(int argc, char * argv[])
+{
+  
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<SystemInterface>());
+  rclcpp::shutdown();
+  
+  return 0;
+}
